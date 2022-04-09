f9b5859061961a1c6a3d73b1779d965b620a357d
BUPT-PRIV <priv@bupt.edu.cn>
2022-04-09 21:49:26 +0800

    add cihp semseg register

---
+++ mask2former/data/datasets/register_cihp_semseg.py
@@ -0,0 +1,43 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import json
+import logging
+import numpy as np
+import os
+from PIL import Image
+
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.utils.file_io import PathManager
+
+
+CIHP_SEM_SEG_CATEGORIES = [
+    "background", "Hat", "Hair", "Gloves", "Sunglasses", "UpperClothes", "Dress", "Coat", "Socks", "Pants",
+    "Torso-skin", "Scarf", "Skirt", "Face", "Left-arm", "Right-arm", "Left-leg", "Right-leg", "Left-shoe", "Right-shoe",
+]
+
+# ==== Predefined splits for raw CIHP images ===========
+_RAW_CIHP_SPLITS = {
+    "cihp_semseg_train":("Training/Images/", "Training/Category_ids/"),
+    "cihp_semseg_val":("Validation/Images/", "Validation/Category_ids/"),
+}
+
+def register_cihp_semseg(root):
+    root = os.path.join(root, "cihp")
+    # print(root)
+    # print('\n\n\n\n\n\n cihp registered in d2')
+    for name, (image_dir, gt_dir) in _RAW_CIHP_SPLITS.items():
+        image_dir = os.path.join(root, image_dir)
+        gt_dir = os.path.join(root, gt_dir)
+        # print(image_dir, gt_dir)
+        DatasetCatalog.register(
+            name, lambda x=image_dir, y=gt_dir: load_sem_seg(y, x, gt_ext="png", image_ext="jpg")
+        )
+        MetadataCatalog.get(name).set(
+            stuff_classes=CIHP_SEM_SEG_CATEGORIES[:],
+            image_root=image_dir,
+            sem_seg_root=gt_dir,
+            evaluator_type="sem_seg",
+            ignore_label=255,
+        )
+
+_root = os.getenv("DETECTRON2_DATASETS", "datasets")
+register_cihp_semseg(_root)
